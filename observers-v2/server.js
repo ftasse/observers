335321ad77b6ff8a0b5124d2d01b40350d27c5ea
AsnelChristian <ngoullasob@gmail.com>
2020-06-04 00:17:58 +0100

    server.js: Handle UNHANDLEDREJECTION

--- observers-v2/server.js
+++ observers-v2/server.js
@@ -29,3 +29,11 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running at port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
