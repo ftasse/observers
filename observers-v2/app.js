323e9ae3f35cd9fbcc3034b921d3b58a0e8b4b8f
AsnelChristian <ngoullasob@gmail.com>
2020-06-04 08:27:37 +0100

    app.js: Add rate-limiting middleware

--- observers-v2/app.js
+++ observers-v2/app.js
@@ -3,11 +3,13 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 
 const globalErrorHandler = require('controllers/errorController');
 const AppError = require('utils/appError');
 
 const app = express();
+const apiEndpoint = '/api/v1';
 
 // Add security HTTP headers to request
 app.use(helmet());
@@ -16,6 +18,13 @@ app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit api requests per IP
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+});
+app.use(apiEndpoint, apiLimiter);
 
 // BodyParser: Parses request's data into req.body
 app.use(express.json());
