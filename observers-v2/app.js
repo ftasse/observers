6ca9e57fa524b4cba6bcbffbb22a59bd5287add0
AsnelChristian <ngoullasob@gmail.com>
2020-06-04 08:11:47 +0100

    app: Add data sanitization middlewares

--- observers-v2/app.js
+++ observers-v2/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const globalErrorHandler = require('controllers/errorController');
 const AppError = require('utils/appError');
@@ -17,6 +19,12 @@ if (process.env.NODE_ENV === 'development') {
 
 // BodyParser: Parses request's data into req.body
 app.use(express.json());
+
+// Data sanitization: Prevents against NoSQL code injection
+app.use(mongoSanitize());
+
+// Data sanitization: Prevents against cross-site scripting
+app.use(xss());
 
 // Handle unimplemented routes
 app.all('*', (req, res, next) => {
