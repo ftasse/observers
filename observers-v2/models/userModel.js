afb5f59f6051b17d0c246bf4a5c026791a6cc04b
AsnelChristian <ngoullasob@gmail.com>
2020-06-05 17:24:21 +0100

    Define (basic) user model
    This model will be improved later on for Oauth.

---
+++ observers-v2/models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+    validate: [validator.isEmail, 'Please provide a valid email']
+  },
+  role: {
+    type: String,
+    default: 'user',
+    enum: ['user', 'admin', 'moderator']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password']
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function(val) {
+        return val === this.password;
+      },
+      message: 'Passwords are not matched'
+    }
+  },
+  thumbnail: String,
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetTokenExpires: Date
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
