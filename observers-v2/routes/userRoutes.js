792fac2c954c90fcbee3622d465db004e904c515
AsnelChristian <ngoullasob@gmail.com>
2020-06-05 18:50:49 +0100

    Authentication: user login

--- observers-v2/routes/userRoutes.js
+++ observers-v2/routes/userRoutes.js
@@ -6,6 +6,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', authController.signup);
+router.post('/signin', authController.signin);
 
 router
   .route('/')
