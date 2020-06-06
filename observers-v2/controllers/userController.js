84f82cf60fd49066b0c617e01fc8f34af36c961c
AsnelChristian <ngoullasob@gmail.com>
2020-06-06 08:32:36 +0100

    Add /updateMe endpoint for updating user

--- observers-v2/controllers/userController.js
+++ observers-v2/controllers/userController.js
@@ -1,10 +1,44 @@
 const User = require('../models/userModel');
 const factory = require('./handlerFactory');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  let updateObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) updateObj[el] = obj[el];
+  });
+  return updateObj;
+};
 
 exports.getMe = (req, res, next) => {
   req.params.id = req.user._id;
   next();
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This is route cannot be used for password update. Please /forgotPassword instead',
+        403
+      )
+    );
+  }
+
+  const filteredObj = filterObj(req.body, 'name', 'email', 'thumbnail');
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredObj, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
 
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
