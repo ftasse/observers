029ef2b9ac998d08dfd52d54b893483d304fca12
AsnelChristian <ngoullasob@gmail.com>
2020-06-11 20:50:47 +0100

    Add tags field to Topic
    Add tags feild to Topic to facilate easy search.

---
+++ observers-v2/models/topicCategories.js
@@ -0,0 +1,15 @@
+module.exports = [
+  'Politics',
+  'Economy',
+  'Health',
+  'Education',
+  'Science',
+  'Lifestyle',
+  'Sports',
+  'Arts, Entertainment, Culture',
+  'Transportation',
+  'Business',
+  'Technology',
+  'Climate & Environment',
+  'Law'
+];
