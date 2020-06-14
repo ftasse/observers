bab04cf3101703c2384e5bba05e2a84231a9af1c
AsnelChristian <ngoullasob@gmail.com>
2020-06-14 17:41:42 +0100

    reportController: Only create report for existing topic

--- observers-v2/controllers/reportController.js
+++ observers-v2/controllers/reportController.js
@@ -1,5 +1,8 @@
 const factory = require('./handlerFactory');
 const Report = require('../models/reportModel');
+const Topic = require('../models/topicModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.setTopicId = (req, res, next) => {
   if (!req.body.topic) req.body.topic = req.params.topicId;
@@ -15,7 +18,18 @@ exports.setUserId = (req, res, next) => {
 
 exports.getAllReport = factory.getAll(Report);
 exports.getReport = factory.getOne(Report);
-exports.createReport = factory.createOne(Report);
+exports.createReport = catchAsync(async (req, res, next) => {
+  const topic = await Topic.findById(req.body.topic);
+  if (!topic) {
+    return next(
+      new AppError(
+        'Attempt to report on a topic that does not exist. Operation can not be performed',
+        404
+      )
+    );
+  }
+  await factory.createOne(Report)(req, res, next);
+});
 exports.updateReport = factory.updateOne(Report);
 exports.deleteReport = factory.deleteOne(Report);
 exports.getReportsWithin = factory.getAllWithin(Report);
