a34d7c2051896988697d1e93163d13d143e3a872
AsnelChristian <ngoullasob@gmail.com>
2020-06-14 17:44:20 +0100

    Add vote model

---
+++ observers-v2/models/voteModel.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const Report = require('../models/reportModel');
+
+const voteSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  report: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Report',
+    required: true
+  },
+  value: {
+    type: Boolean,
+    required: true
+  }
+});
+
+voteSchema.statics.calcLikes = async function(reportId) {
+  const stats = await this.aggregate([
+    { $match: { report: reportId } },
+    {
+      $group: {
+        _id: '$report',
+        numLikes: {
+          $sum: { $cond: [{ $eq: ['$value', true] }, 1, 0] }
+        },
+        numDislikes: {
+          $sum: { $cond: [{ $eq: ['$value', false] }, 1, 0] }
+        }
+      }
+    }
+  ]);
+
+  if (stats.length > 0) {
+    await Report.findByIdAndUpdate(reportId, {
+      numLikes: stats[0].numLikes,
+      numDislikes: stats[0].numDislikes
+    });
+  } else {
+    await Report.findByIdAndUpdate(reportId, {
+      numLikes: 0,
+      numDislikes: 0
+    });
+  }
+};
+
+voteSchema.pre('save', async function(next) {
+  await this.constructor.calcLikes(this.report);
+  next();
+});
+
+voteSchema.pre(/^find(One|ById)And/, async function(next) {
+  this.v = await this.findOne();
+  next();
+});
+
+voteSchema.post(/^find(One|ById)And/, async function() {
+  await this.v.constructor.calcLikes(this.r.report);
+});
+
+const Vote = mongoose.model('Vote', voteSchema);
+
+module.exports = Vote;
