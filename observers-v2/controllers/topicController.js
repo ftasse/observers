1902298b570918716cf2e4d9ca44920f032dbe0f
AsnelChristian <ngoullasob@gmail.com>
2020-06-14 17:14:58 +0100

    Delete all reports when deleting topic

--- observers-v2/controllers/topicController.js
+++ observers-v2/controllers/topicController.js
@@ -1,9 +1,14 @@
 const factory = require('./handlerFactory');
 const Topic = require('../models/topicModel');
+const catchAsync = require('../utils/catchAsync');
+const Report = require('../models/reportModel');
 
 exports.getAllTopics = factory.getAll(Topic);
 exports.getTopic = factory.getOne(Topic, { path: 'reports' });
 exports.createTopic = factory.createOne(Topic);
 exports.updateTopic = factory.updateOne(Topic);
-exports.deleteOne = factory.deleteOne(Topic);
+exports.deleteOne = catchAsync(async (req, res, next) => {
+  await Report.deleteMany({ topic: req.params.id });
+  await factory.deleteOne(Topic)(req, res, next);
+});
 exports.getTopicsWithin = factory.getAllWithin(Topic);
