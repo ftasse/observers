72b260a8c686683d1610c589bca46d2a79779782
AsnelChristian <ngoullasob@gmail.com>
2020-06-17 09:17:16 +0100

    Modify updateVote endpoint to cancel vote

--- observers-v2/controllers/voteController.js
+++ observers-v2/controllers/voteController.js
@@ -1,8 +1,25 @@
 const factory = require('./handlerFactory');
 const Vote = require('../models/voteModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getAllVotes = factory.getAll(Vote);
 exports.getVote = factory.getOne(Vote);
 exports.createVote = factory.createOne(Vote);
-exports.updateVote = factory.updateOne(Vote);
+exports.updateVote = catchAsync(async (req, res, next) => {
+  const vote = await Vote.findOne({ _id: req.params.id, author: req.user.id });
+
+  if (!vote) {
+    return next(new AppError('Vote not found', 404));
+  }
+  vote.value = res.body.value === vote.value ? 0 : res.body.value;
+  vote.save();
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: vote
+    }
+  });
+});
 exports.deleteVote = factory.deleteOne(Vote);
