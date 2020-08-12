27d08c6e5ea79487204d53ae9b3e624f3e8eee44
AsnelChristian <ngoullasob@gmail.com>
2020-08-12 12:02:16 +0100

    Serve login page template

--- observers-v2/routes/viewRoutes.js
+++ observers-v2/routes/viewRoutes.js
@@ -5,6 +5,7 @@ const viewsController = require('../controllers/viewsController');
 const router = new express.Router();
 
 router.get('/', viewsController.getOverview);
-router.get('/topic', viewsController.getTopic);
+router.get('/topics/:slug', viewsController.getTopic);
+router.get('/login', viewsController.login);
 
 module.exports = router;
