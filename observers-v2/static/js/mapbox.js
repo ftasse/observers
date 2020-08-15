68c9748c798d0c45b2fc05824c6154daef661683
AsnelChristian <ngoullasob@gmail.com>
2020-08-15 01:26:03 +0100

    Allow filtering by tags and categories and fix map view of topic

--- observers-v2/static/js/mapbox.js
+++ observers-v2/static/js/mapbox.js
@@ -1,166 +1,60 @@
-import Supercluster from 'supercluster';
-
 const getPopupHTML = topic => {
   return `
     <div class="sm-card">
         <img src="img/politics.jpg" alt="" class="sm-card__img">
         <div class="sm-card__info">
             <div class="sm-card__main">
-                <a href="#" class="link-info link-info--normal">${topic.title}</a>
+                <a href="/topics/${
+                  topic.slug
+                }" class="link-info link-info--normal">${topic.title}</a>
             </div>
             <div class="sm-card__sub">
-                <a href="#" class="sm-card__category sm-card__link link-info link-info--category link-info--category--small">
+                <a href="/?category=${encodeURIComponent(
+                  topic.category
+                )}#topics" class="sm-card__category sm-card__link link-info link-info--category link-info--category--small">
                     ${topic.category}
                  </a>
-                <a href="#" class="sm-card__link link-info link-info--small link-info--small--sm">
-                    Last update on ${topic.createdAt}
+                <a href="/?createdAt=${
+                  new Date(topic.createdAt).toISOString().split('T')[0]
+                }#topics" class="sm-card__link link-info link-info--small link-info--small--sm">
+                    Created on ${new Date(topic.createdAt).toLocaleString(
+                      'en-us',
+                      {
+                        month: 'long',
+                        day: 'numeric',
+                        year: 'numeric'
+                      }
+                    )}
                  </a>
             </div>
         </div>
     </div>`;
 };
-const getClusterElementClass = size => {
-  let min, max;
-  const classMap = {
-    'marker--cluster--20': [2, 20],
-    'marker--cluster--50': [20, 50],
-    'marker--cluster--100': [50, 100],
-    'marker--cluster--500': [100, 500],
-    'marker--cluster--1000': [500, 1000],
-    'marker--cluster--10000': [1000, 10000],
-    'marker--cluster--inf': [10000, Infinity]
-  };
-  const between = (val, min, max) => val >= min && val < max;
 
-  for (let klass of Object.keys(classMap)) {
-    [min, max] = classMap[klass];
-    if (between(size, min, max)) {
-      return klass;
+export const displayMap = (mapObj, data) => {
+  L.tileLayer(
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    {
+      maxZoom: 17,
+      id: 'asnelchristian/ckawh4dvvehmi1io10tvsjvtk',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        'pk.eyJ1IjoiYXNuZWxjaHJpc3RpYW4iLCJhIjoiY2thd2Z0aXJ1MDdteTJ0bXU5NDU2bHZ4MCJ9.kChV328HGtNtda8XQQc4tA'
     }
-  }
-  return '';
-};
+  ).addTo(mapObj);
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiYXNuZWxjaHJpc3RpYW4iLCJhIjoiY2thd2Z0aXJ1MDdteTJ0bXU5NDU2bHZ4MCJ9.kChV328HGtNtda8XQQc4tA';
-
-export const displayMap = (mapObj, bounds, data) => {
-  data.forEach(d => {
-    bounds.extend(d.geometry.coordinates);
+  const markers = L.markerClusterGroup({
+    polygonOptions: { color: '#ba9545' }
   });
 
-  const index = new Supercluster({
-    log: true,
-    radius: 20,
-    extent: 256,
-    maxZoom: 7
-  }).load(data);
-
-  console.log(index);
-  let markers = [];
-
-  const update = zoom => {
-    // Remove all markers currently on the map
-    markers.forEach(marker => marker.remove());
-    markers = [];
-
-    // Compute the new clusters based on zoom level
-    let zoomLevel = zoom || parseInt(mapObj.getZoom());
-    const { _sw, _ne } = bounds;
-    const bbox = [_sw.lng, _sw.lat, _ne.lng, _ne.lat];
-    let clusters = [];
-
-    if (bbox[0] > bbox[2]) {
-      // here we need to get clusters two times:
-      // when there are negative values
-      bbox[0] = _sw.lng - 360;
-      clusters = index.getClusters(bbox, zoomLevel);
-      // ...and positive ones.
-      bbox[0] = _sw.lng;
-      bbox[2] = _ne.lng + 360;
-      clusters = clusters.concat(index.getClusters(bbox, zoomLevel));
-    } else {
-      clusters = index.getClusters(bbox, zoomLevel);
-    }
-
-    console.log(clusters);
-    // Create appropriate markers for each clusters
-    clusters.forEach(cluster => {
-      let popupHtml = '';
-      const el = document.createElement('div');
-      el.classList.add('marker');
-
-      const marker = new mapboxgl.Marker({
-        element: el,
-        anchor: 'center'
-      }).setLngLat(cluster.geometry.coordinates);
-
-      if (cluster.properties && cluster.properties.cluster) {
-        el.classList.add('marker--cluster');
-        el.classList.add(
-          getClusterElementClass(cluster.properties.point_count)
-        );
-        el.innerHTML = `<span>${cluster.properties.point_count_abbreviated}</span>`;
-
-        const expansionZoom = index.getClusterExpansionZoom(
-          cluster.properties.cluster_id
-        );
-        if (zoomLevel >= index.options.maxZoom) {
-          const leaves = index.getChildren(cluster.properties.cluster_id);
-          leaves.forEach(leave => {
-            popupHtml = `${popupHtml}${getPopupHTML(leave)}`;
-          });
-
-          el.removeEventListener('click', function() {});
-          el.addEventListener('click', function() {
-            clusterList.innerHTML = popupHtml;
-            clusterListContainer.classList.remove('hide');
-          });
-        } else {
-          el.addEventListener('click', function(e) {
-            flyTo(cluster.geometry.coordinates, expansionZoom);
-          });
-        }
-      } else {
-        if (cluster.rank && cluster.rank <= 5) {
-          el.classList.add('marker--trending');
-        } else {
-          el.classList.add('marker--default');
-        }
-        popupHtml = getPopupHTML(cluster);
-        addPopup(marker, popupHtml, 'topic-popup');
-      }
-      markers.push(marker);
-    });
-    // Add all newly created markers to map
-    markers.forEach(marker => marker.addTo(mapObj));
-  };
-
-  const addPopup = (marker, popupHtml, className) => {
-    marker.setPopup(
-      new mapboxgl.Popup({ className }).setHTML(popupHtml).setMaxWidth('34rem')
-    );
-  };
-
-  let ready = false;
-
-  mapObj.on('load', () => {
-    ready = false;
-    update(4);
-    ready = true;
+  data.map(d => {
+    const markerIcon = L.divIcon({ className: 'marker marker--default' });
+    const marker = L.marker(d.location.coordinates.reverse(), {
+      icon: markerIcon
+    }).bindPopup(getPopupHTML(d));
+    markers.addLayer(marker);
   });
 
-  mapObj.on('moveend', async () => {
-    if (!ready) return;
-    ready = false;
-    update();
-    ready = true;
-  });
-
-  const flyTo = (center, zoom) => {
-    if (!ready) return;
-    ready = false;
-    mapObj.flyTo({ center, zoom });
-    ready = true;
-  };
+  mapObj.addLayer(markers);
 };
