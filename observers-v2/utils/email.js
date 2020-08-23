f502417daec9be58c163e6d18c9a98768025b41f
AsnelChristian <ngoullasob@gmail.com>
2020-08-23 15:46:30 +0100

    Fix sending email in production

--- observers-v2/utils/email.js
+++ observers-v2/utils/email.js
@@ -11,17 +11,14 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-    } else {
-      return nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD
-        }
-      });
-    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
   }
 
   async send(template, subject) {
