86754f5f5148f22849abed9746c597c89789fda0
AsnelChristian <ngoullasob@gmail.com>
2020-12-26 18:46:07 +0100

    Require report to be authored and deletable by topic's creator

--- observers-v2/static/js/deleteReport.js
+++ observers-v2/static/js/deleteReport.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 import { hideAlert, showAlert, showConfirm } from './alert';
 import { hideLoader, showLoader } from './loader';
 
-const deleteBtns = document.querySelectorAll('.delete-report__btn');
-
-const deleteReport = async reportId => {
+export const deleteReport = async reportId => {
   try {
     showLoader();
     const res = await axios({
@@ -23,11 +21,3 @@ const deleteReport = async reportId => {
     showAlert('failed', err.response.data.message);
   }
 };
-
-deleteBtns.forEach(b => {
-  b.addEventListener('click', e => {
-    showConfirm('Are you sure you want to delete this report?', () => {
-      deleteReport(b.dataset.report);
-    });
-  });
-});
